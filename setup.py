9d1b56846477ddf46fe91bf6bb0e8e2523160583
Harsh Agarwal <harshag.code@gmail.com>
2024-11-04 12:44:31 +0530

    'RAG-done'

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Gyno chatbot",
+    version="0.0.0",
+    author="Harsh Agarwal",
+    description="Chatbot for gyne related queries",
+    author_email="harshagarwal9835@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
--- template.py
+++ template.py
@@ -1,29 +1,21 @@
 import os
-
-app_folder = "gyn_genai"
+from pathlib import Path
 
 files = [
-    f"{app_folder}/__init__.py",
-    f"{app_folder}/config",
-    f"{app_folder}/config/__init__.py",
-    f"{app_folder}/config/config.py",
-    f"{app_folder}/config/utils.py",
-    f"{app_folder}/data",
-    f"{app_folder}/data/__init__.py",
-    f"{app_folder}/data/data_loader.py",
-    f"{app_folder}/data/preprocessing.py",
-    f"{app_folder}/api",
-    f"{app_folder}/api/__init__.py",
-    f"{app_folder}/api/api_endpoints.py",
-    f"{app_folder}/models",
-    f"{app_folder}/models/__init__.py",
-    f"{app_folder}/models/model.py",
-    f"{app_folder}/models/tokenizer.py",
-
-    "tests/__init__.py",
-    "tests/test_app.py",
-    "tests/test_data.py",
-    "tests/test_model.py",
-
-    "main.py"
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    "app.py",
+    "setup.py"
 ]
+
+for filepath in files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+
+    if (not os.path.exists(filepath) or (os.path.getsize(filepath)==0)):
+        with open(filepath, 'w') as f:
+            pass
